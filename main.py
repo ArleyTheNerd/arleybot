7b86110b5dd7451d38cf60835b12b7c29c972f4b
ArleyTheNerd <87275311+ArleyTheNerd@users.noreply.github.com>
2021-07-11 19:09:27 +0100

    Code so far

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import discord
 import os
+import random
 from keep_alive import keep_alive
 from discord.ext import commands
 
@@ -75,6 +76,35 @@ async def unmute(ctx, *, member : discord.Member, reason=None):
 async def clear(ctx, amount=1):
   await ctx.channel.purge(limit=amount)
   await ctx.send(f'Cleared {amount} messages')
+
+@client.command()
+async def ping(ctx):
+  await ctx.send(f'Pong! {client.latency * 1000}ms')
+
+@client.command(aliases=['8ball', 'eightball', '8-ball'])
+async def _8ball(ctx, *, question):
+  responses=["It is certain.",
+            "It is decidedly so.",
+            "Without a doubt.",
+            "Yes - definitely.",
+            "You may rely on it.",
+            "As I see it, yes.",
+            "Most likely.",
+            "Outlook good.",
+            "Yes.",
+            "Signs point to yes.",
+            "Reply hazy, try again.",
+            "Ask again later.",
+            "Better not tell you now.",
+            "Cannot predict now.",
+            "Concentrate and ask again.",
+            "Don't count on it.",
+            "My reply is no.",
+            "My sources say no.",
+            "Outlook not so good.",
+            "Very doubtful."]
+  await ctx.send(f'Question: {question}\nAnswer: {random.choice(responses)}')
+
 
 keep_alive()
 client.run(os.getenv('TOKEN'))
